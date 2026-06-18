fa6d14d27ba5ea6dae3a14316d524715693c85c7
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2026-06-18 10:45:54 -0500

    Update ml_H2O_perm_coef.py

--- ml_H2O_perm_coef.py
+++ ml_H2O_perm_coef.py
@@ -342,7 +342,8 @@ def calc_descriptors(data, smiles_col_pos):
     
     # Loop through each molecule in the dataset
     for pos, row in data.iterrows():
-        molecule_name = row[0]  # Assuming the first column contains the molecule names
+        molecule_name = row.iloc[0]  # Assuming the first column contains the molecule names
+        #molecule_name = row[0]  # Assuming the first column contains the molecule names
         molecule_smiles = row[smiles_col_pos]  # Assuming the specified column contains the SMILES
 
         if pd.isna(molecule_smiles) or molecule_smiles.strip() == '':
